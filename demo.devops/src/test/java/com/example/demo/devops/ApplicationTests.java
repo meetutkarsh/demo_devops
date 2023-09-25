1113426687e590533c9e6ba38fb8febec33b1fcc
Administrator <Administrator@host.docker.internal>
2023-09-25 12:15:05 +0530

    first commit

---
+++ demo.devops/src/test/java/com/example/demo/devops/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
